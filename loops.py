1db47480f7e586fe48c752d0a098096a051f06ae
ClaudWatari <claudwatari95@gmail.com>
2023-02-18 13:30:33 +0300

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,31 @@
+# loops are used to iterate over a list of data.
+# a list is a collection of items, for example, students in a class.
+# using this example, we might have a looping function that calculates the fee balances for each student, or we want to collectively update some information about them
+# there are multiple methods of writing loops, but the most efficient and also popular method uses the keyword "for", followed by the criteria, for example "in" "of" etc.
+# the concept is to iterate over individuals "in" a list, or individuals "of" a list.
+
+# syntax is like
+
+# for item starts_at  1 ends_at 10
+# do something 10 times
+
+# Or
+#while item doesn't_meet_condition
+#do something and modify value of item
+
+For example:
+
+list =["item_one", "item_two, "item_three", "item_four"]
+for item in list:
+    print(item) #or do something with this item
+
+# or
+
+for item of list:
+    print(item) # or whatever you want
+
+#or
+while item < 2:
+    print(item)
+    item = item - 1 # or whatever case you want as your modifier
+
